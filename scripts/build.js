3da4859eb851401d3338d99f66a3735214433867
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2026-03-17 23:43:00 +0800

    docs: add missing `6.2.239` source

--- scripts/build.js
+++ scripts/build.js
@@ -4,23 +4,10 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const README_FILE = 'README.md';
 
-/** Contents that will be removed from the README file when building a GitHub Pages. */
-const EXCLUDE_FROM_PAGES = [
+let content = readFileSync(README_FILE, 'utf8');
+content = content.replaceAll(
   'https://cdn.cnbj1.fds.api.mi-img.com/xiaoqiang/',
-  '(original lost)',
-];
-EXCLUDE_FROM_PAGES.forEach((content) => removeAll(README_FILE, content));
-
-/**
- * Removes all occurrences of `content` from the file.
- *
- * @param {string} file - The path to the file.
- * @param {string} content - The content to remove.
- */
-function removeAll(file, content) {
-  const fileContent = readFileSync(file, { encoding: 'utf8' });
-
-  // Use `split` instead of `replace` to avoid escaping.
-  const newContent = fileContent.split(content).join('');
-  writeFileSync(file, newContent, { encoding: 'utf8' });
-}
+  ''
+);
+content = content.replaceAll('c0d19_6.2.239_ispver-6.0.10', '6.2.239_ispver');
+writeFileSync(README_FILE, content, 'utf8');
